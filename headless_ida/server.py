afe17cbd6ccc9cf26010a63d71712cc91c13bd16
Han Dai <github@daihan.me>
2023-07-19 15:22:40 -0400

    compatible with windows

--- headless_ida/server.py
+++ headless_ida/server.py
@@ -10,32 +10,33 @@ from .helpers import escape_path
 def HeadlessIdaServer(idat_path):
     class _HeadlessIdaServer(rpyc.Service):
         def exposed_init(self, binary):
-            with tempfile.NamedTemporaryFile() as binary_file:
-                binary_file.write(binary)
-                binary_file.flush()
-                binary_path = binary_file.name
+            binary_file = tempfile.NamedTemporaryFile(delete=False)
+            binary_file.write(binary)
+            binary_file.flush()
+            binary_file.close()
+            binary_path = binary_file.name
 
-                server_path = os.path.join(os.path.realpath(
-                    os.path.dirname(__file__)), "ida_script.py")
-                port = 8000
-                with socket.socket() as s:
-                    s.bind(('', 0))
-                    port = s.getsockname()[1]
-                tempidb = tempfile.NamedTemporaryFile(suffix=".idb")
-                p = subprocess.Popen(
-                    f'{idat_path} -o"{tempidb.name}" -A -S"{escape_path(server_path)} {port}" -P+ {binary_path}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                while True:
-                    if p.poll() is not None:
-                        raise Exception(
-                            f"IDA failed to start: return code {p.poll()}\n"
-                            f"=============== STDOUT ===============\n{p.stdout.read().decode()}"
-                            f"=============== STDERR ===============\n{p.stderr.read().decode()}"
-                        )
-                    try:
-                        self.conn = rpyc.connect("localhost", port)
-                    except:
-                        continue
-                    break
+            server_path = os.path.join(os.path.realpath(
+                os.path.dirname(__file__)), "ida_script.py")
+            port = 8000
+            with socket.socket() as s:
+                s.bind(('', 0))
+                port = s.getsockname()[1]
+            tempidb = tempfile.NamedTemporaryFile(suffix=".idb")
+            p = subprocess.Popen(
+                f'{idat_path} -o"{tempidb.name}" -A -S"{escape_path(server_path)} {port}" -P+ {binary_path}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            while True:
+                if p.poll() is not None:
+                    raise Exception(
+                        f"IDA failed to start: return code {p.poll()}\n"
+                        f"=============== STDOUT ===============\n{p.stdout.read().decode()}"
+                        f"=============== STDERR ===============\n{p.stderr.read().decode()}"
+                    )
+                try:
+                    self.conn = rpyc.connect("localhost", port)
+                except:
+                    continue
+                break
 
         def exposed_import_module(self, mod):
             return self.conn.root.import_module(mod)
