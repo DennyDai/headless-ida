9ea6ec57f9686ccb1d5b504fc02052200d793a17
Han Dai <github@daihan.me>
2024-12-08 03:02:58 -0500

    fix dependency

--- src/headless_ida/__init__.py
+++ src/headless_ida/__init__.py
@@ -1,7 +1,8 @@
 from importlib import metadata
 
-from .cli import headlessida_cli, headlessida_server_cli
 from .client import HeadlessIda, HeadlessIdaRemote
 from .server import HeadlessIdaServer
+
+from .cli import headlessida_cli, headlessida_server_cli
 
 __version__ = metadata.version("headless-ida")
